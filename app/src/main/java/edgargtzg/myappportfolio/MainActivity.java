38097a200193e7e3bcfa600b3c8c16c6c7b93356
Edgar Gtz <mexystuff@gmail.com>
2015-07-18 15:59:20 -0500

    Replacing deprecated API from MainActivity.

--- app/src/main/java/edgargtzg/myappportfolio/MainActivity.java
+++ app/src/main/java/edgargtzg/myappportfolio/MainActivity.java
@@ -1,7 +1,7 @@
 package edgargtzg.myappportfolio;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
